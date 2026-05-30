e13417b6e4f2b3ee9a82b1344de812898e60e040
Đoàn Thị Yến Nhi <NhiDTYCE180492@fpt.edu.vn>
2026-05-31 04:08:21 +0700

    feat: Write Blog / Travel Story and CRUD Blogs

--- src/services/blog.service.js
+++ src/services/blog.service.js
@@ -1,77 +1,97 @@
 const BaseService = require('./base.service');
 const blogModel = require('../models/blog.model');
 const blogLocationModel = require('../models/blogLocation.model');
-const db = require('../config/db');
+const ApiError = require('../utils/ApiError');
+const { httpStatus } = require('../constants');
 
 class BlogService extends BaseService {
 
   async list(query = {}) {
-
-    let sql = `SELECT * FROM blog`;
-
-    const values = [];
-
-    // SEARCH
-    if (query.search) {
-
-      sql += ` WHERE title ILIKE $1 OR content ILIKE $1`;
-
-      values.push(`%${query.search}%`);
-    }
-
-    // SORT
-    switch (query.sort) {
-
-      case 'newest':
-        sql += ` ORDER BY date_created DESC`;
-        break;
-
-      case 'oldest':
-        sql += ` ORDER BY date_created ASC`;
-        break;
-
-      case 'az':
-        sql += ` ORDER BY title ASC`;
-        break;
-
-      case 'popular':
-        sql += ` ORDER BY views DESC`;
-        break;
-
-      default:
-        sql += ` ORDER BY blog_id DESC`;
-    }
-
-    // PAGINATION
-    const page = parseInt(query.page) || 1;
-
-    const limit = parseInt(query.limit) || 10;
-
-    const offset = (page - 1) * limit;
-
-    sql += ` LIMIT ${limit} OFFSET ${offset}`;
-
-    const result = await db.query(sql, values);
-
-    return result.rows;
+    return await this.model.listBlogs(query);
   }
 
-
-  async create(payload) {
+  async create(payload, userId) {
     const { location_ids: locationIds = [], ...blogPayload } = payload;
-    const blog = await this.model.create(blogPayload);
+    const blog = await this.model.create({ ...blogPayload, user_id: userId });
 
-    await Promise.all(locationIds.map((locationId) => blogLocationModel.create({
-      blog_id: blog.blog_id,
-      location_id: locationId,
-    })));
+    await Promise.all(
+      locationIds.map((locationId) =>
+        blogLocationModel.create({
+          blog_id: blog.blog_id,
+          location_id: locationId,
+        })
+      )
+    );
 
     return {
       ...blog,
       location_ids: locationIds,
     };
   }
+
+  async update(id, payload, user) {
+
+    console.log('USER:', user);
+
+    const blog = await this.model.findById(id);
+
+    if (!blog) {
+      throw new ApiError(
+        httpStatus.NOT_FOUND,
+        'Blog not found'
+      );
+    }
+
+    console.log('BLOG:', blog);
+
+    console.log(
+      'BLOG USER ID:',
+      blog.user_id,
+      typeof blog.user_id
+    );
+
+    console.log(
+      'TOKEN USER ID:',
+      user.sub,
+      typeof user.sub
+    );
+
+    if (
+      user.role !== 'admin' &&
+      Number(blog.user_id) !== Number(user.sub)
+    ) {
+      throw new ApiError(
+        httpStatus.FORBIDDEN,
+        'Permission denied'
+      );
+    }
+
+    return await this.model.update(id, payload);
+  }
+
+  async remove(id, user) {
+
+    const blog = await this.model.findById(id);
+
+    if (!blog) {
+      throw new ApiError(
+        httpStatus.NOT_FOUND,
+        'Blog not found'
+      );
+    }
+
+    if (
+      user.role !== 'admin' &&
+      Number(blog.user_id) !== Number(user.sub)
+    ) {
+      throw new ApiError(
+        httpStatus.FORBIDDEN,
+        'Permission denied'
+      );
+    }
+
+    return await this.model.remove(id);
+  }
 }
 
 module.exports = new BlogService(blogModel);
-
