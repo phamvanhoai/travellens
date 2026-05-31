0c2ac3aedc9276c59fe1df9d24eca29584735ec7
Đoàn Thị Yến Nhi <NhiDTYCE180492@fpt.edu.vn>
2026-05-31 21:51:43 +0700

    View review list and Delete review of staff

--- src/routes/review.route.js
+++ src/routes/review.route.js
@@ -1,6 +1,24 @@
 const createCrudRoute = require('./crud.route');
 const controller = require('../controllers/review.controller');
 const { entity } = require('../validators');
+const { authenticate, authorize, } = require('../middlewares/auth.middleware');
 
-module.exports = createCrudRoute(controller, entity.review);
+const router = createCrudRoute(controller, entity.review);
 
+
+// Staff Review Management
+router.get(
+  '/staff/list',
+  authenticate,
+  authorize('staff'),
+  controller.list
+);
+
+router.delete(
+  '/staff/:id',
+  authenticate,
+  authorize('staff'),
+  controller.remove
+);
+
+module.exports = router;
